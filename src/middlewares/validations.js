2578451bea574a4831378d07f9a67a5930969294
Tarick <gabre.tarick@gmail.com>
2022-09-11 14:17:46 -0300

    req04

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -3,7 +3,7 @@ function nameValidation(name) {
     return { type: 400, message: '"name" is required' };
   }
   if (name.length < 5) {
-    return { type: 422, message: '"name" must be at least 5 characters long' };
+    return { type: 422, message: '"name" length must be at least 5 characters long' };
   }
   return { type: null, message: 'ok' };
 }
